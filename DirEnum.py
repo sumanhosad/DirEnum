93dcb31f5a52f2ca4766a41808c3a1bee88faa1f
sumanhosad <sumanhosad98@gmail.com>
2024-06-21 04:18:01 +0530

    removed the use of thread

--- DirEnum.py
+++ DirEnum.py
@@ -1,37 +1,33 @@
 import requests
-import concurrent.futures
 import sys
 
 stop_execution = False
 
 def check_directory(url, directory):
-    """Check if the directory exists on the given URL."""
     global stop_execution
     if stop_execution:
         return
     full_url = url.rstrip('/') + '/' + directory.strip()
-    response = requests.get(full_url)
-    if response.status_code == 200:
-        print(f'[+] {full_url}')
-    elif response.status_code == 403:
-        print(f'[!] Forbidden access: {full_url}')
-    elif response.status_code == 404:
-        pass
+    try:
+        response = requests.get(full_url)
+        if response.status_code == 200:
+            print(f'[+] {full_url}')
+        elif response.status_code == 403:
+            print(f'[!] Forbidden access: {full_url}')
+        elif response.status_code == 404:
+            pass
+    except requests.RequestException as e:
+        print(f'[-] Request failed for {full_url}: {e}')
 
 def enumerate_directory(url):
-    """Enumerate directories on the given URL using the provided wordlist."""
     global stop_execution
     try:
         with open('direnumwordlist.txt', 'r') as file:
             directories = file.readlines()
-            with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-                # Submit tasks to the ThreadPoolExecutor
-                futures = {executor.submit(check_directory, url, directory): directory for directory in directories}
-                # Wait for all tasks to complete
-                for future in concurrent.futures.as_completed(futures):
-                    future.result()
-                    if stop_execution:
-                        sys.exit()
+            for directory in directories:
+                if stop_execution:
+                    sys.exit()
+                check_directory(url, directory)
     except FileNotFoundError:
         print(f'Error: Wordlist file "direnumwordlist.txt" not found.')
     except KeyboardInterrupt:
